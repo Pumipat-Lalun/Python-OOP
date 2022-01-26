f23e4508a666c9ba6fdbfb0bdde12176da2a1abd
Varle0s <68681310+Varle0s@users.noreply.github.com>
2022-01-26 21:38:28 +0700

    Update LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -1,27 +1,27 @@
 
 class Order :
-    def __init__(self,Cup,Flavor,Topping) :
-        self.Cup = Cup
-        self.Flavor = Flavor
-        self.Topping = Topping
+    def __init__(self,cup,flavor,topping) :
+        self.cup = cup
+        self.flavor = flavor
+        self.topping = topping
         self.count = 0
         self.price = 0
         
     
     def getIcecream(self):
-        print("Yor order Ice-cream Size : " + self.Cup)
-        print("Flavor : " + str(self.Flavor))
-        print("Topping : " + str(self.Topping))
+        print("Yor order Ice-cream Size : " + self.cup)
+        print("flavor : " + str(self.flavor))
+        print("topping : " + str(self.topping))
 
     def SetCalculateOrder(self):
-        if self.Cup == 'S':
+        if self.cup == 'S':
             self.price += 15
-        elif self.Cup == 'M':
+        elif self.cup == 'M':
             self.price += 25
-        elif self.Cup == 'L':
+        elif self.cup == 'L':
             self.price += 45
 
-        for i in range(0,len(self.Topping)):
+        for i in range(0,len(self.topping)):
             self.count += 1
         self.count *= 5
         self.price += self.count
@@ -33,18 +33,20 @@ class Order :
        
 
 class Customer :
-    def __init__(self,Crash) :
-        self.Crash = Crash
+    def __init__(self,crash) :
+        self.crash = crash
 
     def SetOrder(self,size,flavor,topping):
         O = Order(size,flavor,topping)
         return O 
 
-    def Payment(self,Price):
-        Return = self.Crash - Price
+    def Payment(self,price):
+        Return = self.crash - price
         print("Return : "+ str(Return) + " Baht")
 
 class Officer :
+    
+
     def GetOrder(self,order):
         print(order.getIcecream())
 
@@ -59,6 +61,11 @@ class Officer :
 
 Customer1 = Customer(100) # you have 100 baht
 Officer1 = Officer()
+Customer2 = Customer(200) # you have 200 baht
+Officer2 = Officer()
+
+
+
 order1 = Customer1.SetOrder('S',['Vanila'],['Cookie','Jelly','Stickyrice'])
 Officer1.GetOrder(order1)
 total1 = Officer1.CalculateOrder(order1)
@@ -66,8 +73,6 @@ Officer1.ShowPrice(order1)
 Customer1.Payment(total1)
 print("\n")
 
-Customer2 = Customer(200) # you have 200 baht
-Officer2 = Officer()
 order2 = Customer2.SetOrder('M',['Vanila','Chocolate'],['Jelly','Pumpkin'])
 Officer2.GetOrder(order2)
 total2 = Officer2.CalculateOrder(order2)
